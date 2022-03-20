64e164571c6f85ec6a7f65f0a8a69c06620318cd
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-20 11:46:04 -0400

    Tests 1&2 ACTUALLY complete

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // code your solution here
 function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ` + activity +'!'
 
 }
 
 function mondayWork(task = 'go to the office') {
-
+    return 'This Monday, I will ' + task + `.`
 }
 
+function wrapAdjective(innerFunction) {
+    return `You are ${innerFunction(par)}`
+}
